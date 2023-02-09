b1589e77ad09146bf8947f4eace2a8bb1473835b
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-09 15:39:43 +0300

    start back

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+
+const express = require('express');
+const db = require('./db/models');
+const config = require('./config/config');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+config(app);
+
+const start = async () => {
+  try {
+    await db.sequelize.authenticate();
+    app.listen(PORT, () => {
+      console.log(`Сервер слушает ${PORT} порт`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+start();
