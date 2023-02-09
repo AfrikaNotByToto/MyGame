47b08b56d73f993d9e8862918a28c8a235bda21b
Slimshady3133 <i.puzakov2010@mail.ru>
2023-02-09 17:56:40 +0300

    frontLogReg

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
+import { useDispatch } from 'react-redux';
+import * as api from './api';
+// import { RootState } from '../store';
 
-function App() {
+function App(): JSX.Element {
+  // const { userState } = useSelector((store: RootState) => store);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    api.checkUser().then((data) =>
+      dispatch({
+        type: 'CHECK_USER',
+        payload: data,
+      })
+    );
+  }, []);
   return (
     <div>
       <h1>привет</h1>
     </div>
-
   );
 }
 
---
+++ frontend/src/features/auth/userReducer.ts
@@ -0,0 +1,32 @@
+/* eslint-disable import/prefer-default-export */
+import Action from './Types/Action';
+import { State } from './Types/types';
+
+const init = {
+  user: {},
+  message: '',
+};
+
+export const userReducer = (state: State = init, action: Action): State => {
+  switch (action.type) {
+    case 'REG_USER':
+      return {
+        ...state,
+        user: action.payload.user,
+        message: action.payload.message,
+      };
+    case 'LOGIN_USER':
+      return {
+        ...state,
+        user: action.payload.user,
+        message: action.payload.message,
+      };
+    case 'CHECK_USER':
+      return {
+        ...state,
+        user: action.payload.user,
+      };
+    default:
+      return state;
+  }
+};
