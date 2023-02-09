2b2fbc4b9a7b849651f158acedabce73f4dd634a
jkantyukova <jkantyukova@gmail.com>
2023-02-09 17:31:39 +0300

    add Main page, add Routes in App

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import MainPage from '../features/Main/MainPage';
+import { Route, Routes } from 'react-router-dom';
+import Header from '../features/Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>привет</h1>
+    <div className="app__container">
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<MainPage />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/authorization" element={<Authorization />} />
+        </Route>
+      </Routes>
     </div>
-
   );
 }
 
