6a88650243f9e6bf11c4096c51139d8bd38a0fff
Tema <rzhevka7@gmail.com>
2023-02-10 13:57:44 +0300

    123

--- frontend/src/features/questions/QuestionCard.tsx
+++ frontend/src/features/questions/QuestionCard.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Topic } from './Types/Types';
 
 export default function QuestionCard({ topic }: { topic:Topic }): JSX.Element {
-  // const [answer, setAnswer] = useState('');
-
   return (
 
     <div>
@@ -14,13 +12,6 @@ export default function QuestionCard({ topic }: { topic:Topic }): JSX.Element {
     //   <form onSubmit={pushAnswer}>
 
     //     <h3>{question.title}</h3>
-    //     <input
-    //       className="answer_input"
-    //       onChange={(e) => setAnswer(e.target.value)}
-    //       value={answer}
-    //       type="text"
-    //     />
-    //     <button type="submit" className="answer_button">ответить</button>
     //   </form>
     //   <p>{}</p>
     // </div>
